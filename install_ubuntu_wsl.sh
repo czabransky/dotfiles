ac26b41d0c399809bf1bdb01df56f63e49974b88
Colin Zabransky <zabransky.colin@gmail.com>
2024-02-14 23:33:08 -0500

    add install script for windows x ubuntu

---
+++ install_ubuntu_wsl.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# configure wsl.conf 
+CURRENT_USER=$USER
+sudo /bin/bash -c 'printf "[user]\ndefault=%s\n" $CURRENT_USER >> /etc/wsl.conf'
+sudo /bin/bash -c 'printf "[boot]\nsystemd=true\n" >> /etc/wsl.conf'
+
+# upgrade distribution
+sudo apt -y upgrade --fix-missing
+sudo apt -y full-upgrade
+
+# install xapps
+sudo apt -y install x11-apps
+
+# brew
+NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+(echo; echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"') >> $HOME/.bashrc
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+sudo apt -y install build-essential
+NONINTERACTIVE=1 /home/linuxbrew/.linuxbrew/bin/brew install gcc
+
+# tmux 
+NONINTERACTIVE=1 /home/linuxbrew/.linuxbrew/bin/brew install tmux
+
+# python
+NONINTERACTIVE=1 /home/linuxbrew/.linuxbrew/bin/brew install python3
+python3 -m pip install --upgrade pip
+
+# zsh
+NONINTERACTIVE=1 /home/linuxbrew/.linuxbrew/bin/brew install zsh
+sudo sh -c 'printf "%s\n" /home/linuxbrew/.linuxbrew/bin/ >> /etc/shells'
+sudo chsh -s /home/linuxbrew/.linuxbrew/bin/zsh
+eval "$(/home/linuxbrew/.linuxbrew/bin/zsh shellenv)"
+/bin/bash/ -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh) --unattended"
+(echo; echo 'source ~/.zshrc.pre-oh-my-zsh') >> $HOME/.zshrc
+omz theme set cloud
+
+# install zoxide and fuzzy finder for zoxide interactive
+brew install zoxide
+eval "$(zoxide init zsh)"
+brew install fzf
