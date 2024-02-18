cc597cc5b44ed0362c60baf0b8c77fc646086b74
Colin Zabransky <zabransky.colin@gmail.com>
2024-02-18 15:43:27 -0500

    add qol theme commands

--- dot-config/nvim/lua/plugins/theme.lua
+++ dot-config/nvim/lua/plugins/theme.lua
@@ -3,12 +3,11 @@
 --vim.cmd.colorscheme 'catppuccin-latte'
 --vim.cmd.colorscheme 'catppuccin-macchiato'
 --vim.cmd.colorscheme 'catppuccin-mocha'
-
 vim.cmd.colorscheme 'gruvbox'
 
-colorscheme 'catppuccin'
-colorscheme 'catppuccin-frappe'
-colorscheme 'catppuccin-latte'
-colorscheme 'catppuccin-macchiato'
-colorscheme 'catppuccin-mocha'
-colorscheme 'gruvbox'
+--colorscheme catppuccin
+--colorscheme catppuccin-frappe
+--colorscheme catppuccin-latte
+--colorscheme catppuccin-macchiato
+--colorscheme catppuccin-mocha
+--colorscheme gruvbox
