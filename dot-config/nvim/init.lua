c2754eafa5d361c568ee6f513b46dc977b0a2fc8
Colin Zabransky <zabransky.colin@gmail.com>
2024-02-18 16:51:16 -0500

    add highlighted yank

--- dot-config/nvim/init.lua
+++ dot-config/nvim/init.lua
@@ -1,7 +1,29 @@
+-- [[ README  
+--	for plugins to operate you will need:
+--		ripgrep: https://github.com/BurntSushi/ripgrep
+
 -- Load vimrc first, then override with nvim config.
 vim.cmd([[ source ~/.vimrc ]])
+vim.g.netrw_browse_split = 0
+vim.g.netrw_banner = 1
+vim.g.netrw_winsize = 25
+vim.o.clipboard = 'unnamedplus'
+--vim.o.completeopt = 'menuone, noselect'
 
 require('plugins.lazy')
 require('plugins.theme')
 require('plugins.whichkey')
 require('plugins.telescope')
+
+-- [[ Highlight on Yank ]]
+-- see `:help vim.highlight.on_yank()`
+local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
+vim.api.nvim_create_autocmd('TextYankPost', {
+	callback = function()
+		vim.highlight.on_yank({
+			timeout = 400
+		})
+	end,
+	group = highlight_group,
+	pattern = "*",
+})
--- dot-config/nvim/lua/plugins/whichkey.lua
+++ dot-config/nvim/lua/plugins/whichkey.lua
@@ -1,3 +1,13 @@
-require('which-key').register ({
+require('which-key').setup {
+	icons = {
+		separator = "-> "
+	}
+}
+
+require('which-key').register {
+	['<leader>w'] = { name = '[W]indows' },
 	['<leader>s'] = { name = '[S]earch' },
-})
+	['<leader>g'] = { name = '[G]it' },
+}
+
+vim.keymap.set('n', '<leader>q', ':q', { desc = 'Quit' })
