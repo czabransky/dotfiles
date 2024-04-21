e69e8171c5862764ccddfc3e121e7907dea62e88
Colin Zabransky <zabransky.colin@gmail.com>
2024-04-21 09:21:30 -0400

    add markdown, not sure i'll use it yet

--- .config/nvim/lua/colin/init.lua
+++ .config/nvim/lua/colin/init.lua
@@ -2,6 +2,6 @@ require('colin.core.options').setup({
 	inherit_vimrc = true,
 	leader = ' ',
 })
-require('colin.core.autocmd')
 require('colin.core.keymaps')
+require('colin.core.autocmd')
 require('colin.lazy')
